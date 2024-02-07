213ec19d67278fea80ce4dd68df70be43d203f51
Niklas Röttger <niklas.roettger@outlook.de>
2024-02-07 14:51:36 +0100

    add demo code

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react';
+
 export default function Joke() {
+  const [joke, setJoke] = useState();
+  const [jokeId, setJokeId] = useState(0);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        `https://example-apis.vercel.app/api/bad-jokes/${jokeId}`
+      );
+      const fetchedJoke = await response.json();
+
+      setJoke(fetchedJoke);
+    }
+
+    startFetching();
+  }, [jokeId]);
+
+  if (!joke) {
+    return null;
+  }
+
   return (
     <>
-      <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <h1>{joke.joke}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setJokeId(joke.nextId);
+        }}
+      >
+        Next Joke
+      </button>
     </>
   );
 }
